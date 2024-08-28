100ea5c09d862f40a831760d6453d054792ef321
Joshua Ross <josh@rossjm.dev>
2024-08-28 12:02:59 -0400

    name change of scripts and added readme in shell script section on how to use said scripts

---
+++ shell/readme.md
@@ -0,0 +1,36 @@
+<h1 align="center"> Shell Scipts </h1>
+
+<details>
+<summary> Shell Scripts Here! </summary>
+
+### Powersheller
+
+- Install Powershell on Linux
+
+```shell
+curl -s https://raw.githubusercontent.com/coloredbytes/jershs-scripts/main/shell/powersheller.sh | bash
+```
+
+### Push 2 Git
+
+- Push Local Repos To GitHub or any cloud-repo site.
+
+```shell
+# Pull Script
+curl -O https://raw.githubusercontent.com/coloredbytes/jershs-scripts/main/shell/push-to-github.sh
+
+# Run Script
+./push-to-github.sh
+```
+### Sig Create
+
+-  Create Signature Header for shell and PowerShell scripts.
+
+```shell
+# Pull Script
+curl -o https://raw.githubusercontent.com/coloredbytes/jershs-scripts/main/shell/sig-create.sh
+
+# Run Script
+./sig-create.sh
+```
+</details>
--- shell/sig-create.sh
+++ shell/sig-create.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
 # Prompt for user input
-read -p "What is the script name? " scriptname
-read -p "What is the purpose of the script? " purpose
+read -rp "What is the script name? " scriptname
+read -rp "What is your name?" author_name
+read -rp "What is your GitHub profile URL?" profile_url
+read -rp "What is the purpose of the script? " purpose
+
 
 # Define additional variables
-author="Joshua Ross"
-githubUrl="https://github.com/ColoredBytes"
-license="MIT License (if applicable)"
 currentDateTime=$(date +"%m/%d/%Y")
 
 # Create the output string
 outputString=$(cat <<EOF
 # =============================================================================
 # Script Name: $scriptname
-# Author: $author
-# GitHub: $githubUrl
+# Author: $author_name
+# GitHub: $profile_url
 # Purpose: $purpose
 # Date: $currentDateTime
-# License: $license
 # =============================================================================
 EOF
 )
@@ -26,9 +25,5 @@ EOF
 # Output the final string
 echo "$outputString"
 
-# Copy the final string to the clipboard (requires xclip or pbcopy depending on the OS)
-# Uncomment the appropriate line based on your OS:
-# echo "$outputString" | xclip -selection clipboard   # For Linux with xclip
-# echo "$outputString" | pbcopy                       # For macOS with pbcopy
 
 
