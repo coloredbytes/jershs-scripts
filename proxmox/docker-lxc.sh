73e6b0096014a8f41211870157da74b00f504a1c
Joshua Ross <josh@rossjm.dev>
2024-10-02 11:59:28 -0400

    Create docker-lxc.sh

---
+++ proxmox/docker-lxc.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+# =============================================================================
+# Script Name: docker-lxc
+# Author: Joshua Ross
+# Purpose: Install Docker on Proxmox LXC.
+# Date: 10/02/2024
+# =============================================================================
+
+### Functions ###
+docker_install() {
+  curl -fsSL https://get.docker.com -o get-docker.sh
+}
+
+add_user() {
+  read -rp "What is the user?" CREATED_USER
+  adduser "$CREATED_USER"
+}
+
+### Main Script ###
+add_user
+docker_install
+
+### Add User to Docker Group ###
+usermod -aG docker "CREATED_USER"
+newgrp docker
+
+
