b35a3299f9e2c776d5e443e55f7b7f7e0cf641e3
Joshua Ross <josh@rossjm.dev>
2024-10-02 12:31:49 -0400

    fixed the docker-lxc script

--- proxmox/docker-lxc.sh
+++ proxmox/docker-lxc.sh
@@ -8,22 +8,33 @@
 # =============================================================================
 
 ### Functions ###
+
+system_update() {
+  apt update
+  apt upgrade -y
+}
+
 docker_install() {
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
 }
 
 add_user() {
-  read -rp "What is the user?" CREATED_USER
+  read -rp "What is the user? " CREATED_USER
   adduser "$CREATED_USER"
+  usermod -aG sudo "$CREATED_USER"
 }
 
 ### Main Script ###
+system_update
 add_user
 docker_install
 
 ### Add User to Docker Group ###
-usermod -aG docker "CREATED_USER"
-newgrp docker
+usermod -aG docker "$CREATED_USER"
+
+### Reboot The Server ###
+echo "[+] Rebooting Servier for safe measures..."
+reboot
 
 
